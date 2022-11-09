989b088b8e82cb1e68a889f4a2e290bafa788380
Jules Giraud <jules.giraud@outlook.fr>
2022-11-09 16:00:32 +0100

    first commit

---
+++ src/CurrencyRow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CurrencyRow() {
+  return (
+    <div><input type="text" className={"input"}/>
+        <select name="converter" id="converter">
+            <option value="Hi"></option>
+        </select>
+    </div>
+  )
+}
