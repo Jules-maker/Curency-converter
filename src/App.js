2e5d48ec2a2a678ca14c8b94f8b38c9961b7713c
Jules Giraud <jules.giraud@outlook.fr>
2022-11-11 21:08:56 +0100

    0.0.2 test gitignore

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 import CurrencyRow from './CurrencyRow';
 
 const BASE_URL = 'https://api.apilayer.com/exchangerates_data/symbols'
-function App() {
-
-useEffect(() => {
-  fetch(BASE_URL)
-  .then(res => res.json())
-  .then(data => console.log(data))
-},[])
-// TODO: faire avec exchangeRateAPI
 var myHeaders = new Headers();
 myHeaders.append("apikey", "API_KEY a securiser");
-
 var requestOptions = {
   method: 'GET',
   redirect: 'follow',
   headers: myHeaders
 };
+function App() {
+  
+useEffect(() => {
+  fetch(BASE_URL,requestOptions)
+  .then(res => res.json())
+  .then(data => console.log(data))
+},[])
+// TODO: faire avec exchangeRateAPI
+
 
 fetch("https://api.apilayer.com/exchangerates_data/convert?to=EUR&from=GBP&amount=10", requestOptions)
   .then(response => response.text())
